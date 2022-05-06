947ec9130c8b5d358126a5943154bfbe1e20a01a
Randy Paffenroth <randy.paffenroth@gmail.com>
2022-05-06 06:40:55 -0400

    Small changes

--- GETSSH.sh
+++ GETSSH.sh
@@ -4,7 +4,7 @@
 
 export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
 rm -rf $HOME/.ssh.bak $HOME/ssh 
-git clone rcpaffenroth@haven.rcpaffenroth.org:repos/ssh.git $HOME/ssh
+git clone rcpaffenroth@moc-gateway.rcpaffenroth.org:repos/ssh.git $HOME/ssh
 mv $HOME/.ssh $HOME/.ssh.bak
 mv $HOME/ssh $HOME/.ssh
 sh $HOME/.ssh/permissions.sh
