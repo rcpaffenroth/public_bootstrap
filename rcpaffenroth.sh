e068ad8760df94e765320656bf8fc6c3ef24d133
Randy Paffenroth <randy.paffenroth@gmail.com>
2022-05-11 13:55:27 -0400

    Cleaned things up a bit including using existing ansible if we can.

--- rcpaffenroth.sh
+++ rcpaffenroth.sh
@@ -2,6 +2,7 @@
 
 # This script can be run by the rcpaffenroth user to do a basic setup.
 # First we get .ssh
+cd $HOME
 rsync -av rcpaffenroth@haven.rcpaffenroth.org:.ssh .ssh.new
 mv .ssh .ssh.old
 mv .ssh.new .ssh
@@ -10,9 +11,17 @@ mv .ssh.new .ssh
 rsync -av rcpaffenroth@haven.rcpaffenroth.org:.rcp .
 
 # Next we get the rest of the stuff using ansible
-mkdir -p projects
-cd projects
-git clone git@bitbucket.org:rcpaffenroth/ansible.git
-cd ansible
+if [ -d "/path/to/dir" ] 
+then
+    echo "Using current ansible" 
+    cd projects/ansible
+else
+    echo "Downloading ansible"
+    mkdir -p projects
+    cd projects
+    git clone git@bitbucket.org:rcpaffenroth/ansible.git
+    cd ansible
+fi
+
 bin/update_local_system
 bin/update_local_rcpafferoth
