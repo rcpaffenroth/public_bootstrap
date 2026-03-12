e1b10a6fa04b4011aee24ff8444a758851a7da04
Randy Paffenroth <randy.paffenroth@gmail.com>
2026-03-12 16:02:20 -0400

    Cleaned up

--- rcpaffenroth.sh
+++ rcpaffenroth.sh
@@ -3,10 +3,6 @@
 # This script can be run by the rcpaffenroth user to do a basic setup.
 # First we get ssh keys and configuration
 cd $HOME
-rsync -av rcpaffenroth@setup.rcpaffenroth.org:.ssh_rcp .
-bash .ssh_rcp/setup.sh
-
-# Next we get .rcp with the keys in there
 rsync -av rcpaffenroth@setup.rcpaffenroth.org:.rcp .
 
 # Next we get the rest of the stuff using ansible
@@ -16,8 +12,6 @@ eval `ssh-agent -s`
 ssh-add
 # Note, rcpaffenroth does not necessarily have passwordless sudo at this point.
 cd ansible
-# bash setup_venv.sh
-# . venv/bin/activate
 ansible-galaxy collection install -r requirements.yml
 ansible-playbook -i inventory/localhost.ini playdir/system_setup.yml
 ansible-playbook -i inventory/localhost.ini playdir/rcpaffenroth_setup.yml
